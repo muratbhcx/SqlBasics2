d7dcd18f11a566c4147bc606498ef94eb4030736
Murat <muratbehucbuga@gmail.com>
2025-04-12 20:09:39 +0300

    SQL TEMELLERİ 2

---
+++ Odev6.java
@@ -0,0 +1,17 @@
+package Hafta_6.Sql_Temelleri_2;
+
+public class Odev6 {
+
+    //film tablosunda bulunan rental_rate sütunundaki değerlerin ortalamasını gösterir.
+    SELECT ROUND (AVG(rental_rate),2) FROM film;
+
+    //film tablosunda bulunan filmlerden kaç tanesin 'C' karakteri ile başladığını gösterir.
+    SELECT COUNT (*) FROM film WHERE title LIKE 'C%';
+
+    //film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakika olduğunu gösterir.
+    SELECT MAX (length) FROM film WHERE rental_rate = 0.99;
+
+    //film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri olduğunu gösterir.
+    SELECT COUNT (DISTINCT replacement_cost) FROM film WHERE length > 150;
+
+}
